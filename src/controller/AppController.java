a0997c544049f9e33c1aa29534c33cd20be2ec84
abd alrahman elhabashy <elhabashy2026@gmail.com>
2026-04-16 11:09:49 +0200

    refactor: change AppController methods to static to match team architecture

--- src/controller/AppController.java
+++ src/controller/AppController.java
@@ -7,49 +7,42 @@ import data.FoodData;
 
 public class AppController {
 
-    private final ZakatService zakatService = new ZakatService();
-    private final ConsoleHelper consoleHelper = new ConsoleHelper();
-    private final MainScreen mainScreen = new MainScreen();
-    private final InputScreen inputScreen = new InputScreen();
-    private final MenuScreen menuScreen = new MenuScreen();
-    private final ResultScreen resultScreen = new ResultScreen();
-
-    public void run() {
+    public static void run() {
         showWelcomeScreen();
         boolean shouldContinue;
         do {
             processZakatCycle();
             System.out.println();
             System.out.print("Do you want to try again? (1: Yes / 2: No): ");
-            shouldContinue = (consoleHelper.readIntInRange(1, 2) == 1);
+            shouldContinue = (ConsoleHelper.readIntInRange(1, 2) == 1);
         } while (shouldContinue);
     }
 
-    private void showWelcomeScreen() {
+    private static void showWelcomeScreen() {
         // ** welcome screen **
-        consoleHelper.clear();
-        mainScreen.showGreeting();
-        consoleHelper.pause();
+        ConsoleHelper.clear();
+        MainScreen.show();
+        ConsoleHelper.pause();
     }
 
-    private void processZakatCycle() {
+    private static void processZakatCycle() {
         // ** Read number of family members **
-        consoleHelper.clear();
-        inputScreen.show();
-        int familyMembers = consoleHelper.readInt();
+        ConsoleHelper.clear();
+        InputScreen.show();
+        int familyMembers = ConsoleHelper.readInt();
 
         // ** show food category menu **
-        consoleHelper.clear();
-        menuScreen.show();
-        int choice = consoleHelper.readIntInRange(1, FoodData.getList().size());
+        ConsoleHelper.clear();
+        MenuScreen.show();
+        int choice = ConsoleHelper.readIntInRange(1, FoodData.getList().size());
         // ** Get kg per person chosen food **
         double kgPerPerson = FoodData.getList().get(choice - 1).kiloPerSa3;
 
         // **Calculate total zakat weight**
-        double totalKg = zakatService.calculate(familyMembers, kgPerPerson);
+        double totalKg = ZakatService.calculate(familyMembers, kgPerPerson);
 
         // ** Show result **
-        consoleHelper.clear();
-        resultScreen.show(totalKg);
+        ConsoleHelper.clear();
+        ResultScreen.show(totalKg);
     }
 }
